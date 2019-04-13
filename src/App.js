959fd69384f2b8887246566ae7f40e78164fb199
Aditya Pratap Singh Rajput <apsrcreatix@gmail.com>
2019-04-13 13:34:02 +0530

    add starters

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        code goes here.
+      </div>
+    );
+  }
+}
+
+export default App;
